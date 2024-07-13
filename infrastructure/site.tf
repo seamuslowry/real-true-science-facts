e5ee7f35bb341527deea76bcb80a157119ef5c2e
Seamus Lowry <spl1owry3@gmail.com>
2024-07-12 22:01:57 -0400

    feat: add cloudflare

--- infrastructure/site.tf
+++ infrastructure/site.tf
@@ -8,3 +8,26 @@ resource "azurerm_static_web_app" "realtruesciencefacts" {
   resource_group_name = azurerm_resource_group.site.name
   location            = azurerm_resource_group.site.location
 }
+
+resource "azurerm_static_web_app_custom_domain" "realtruesciencefactscustomdomain" {
+  domain_name           = "realtruesciencefacts.com"
+  static_web_app_id     = azurerm_static_web_app.realtruesciencefacts.id
+  validation_type       = "dns-txt-token"
+}
+
+resource "cloudflare_record" "txt_verification" {
+  zone_id = local.cloudflare_zone_id
+  name    = "_dnsauth"
+  type    = "TXT"
+  value   = azurerm_static_web_app_custom_domain.realtruesciencefactscustomdomain.validation_token
+  ttl     = 3600
+}
+
+resource "cloudflare_record" "cname_record" {
+  zone_id = local.cloudflare_zone_id
+  name    = "@"
+  type    = "CNAME"
+  value   = azurerm_static_web_app.realtruesciencefacts.default_host_name
+  ttl     = 1
+  proxied = true
+}
