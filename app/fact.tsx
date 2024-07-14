99a1882cfb8c1881cdecfa32a692030852868536
Seamus Lowry <spl1owry3@gmail.com>
2024-07-14 15:36:06 -0400

    try shuffling as next app

--- app/fact.tsx
+++ app/fact.tsx
@@ -3,15 +3,6 @@ import { useEffect, useState } from 'react';
 import type { Fact } from './page';
 import React from 'react';
 
-function shuffleFacts(facts: Fact[]) {
-  const newArrray = [...facts];
-  for (let i = newArrray.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [newArrray[i], newArrray[j]] = [newArrray[j], newArrray[i]];
-  }
-  return newArrray;
-}
-
 export function FactCard({ fact }: { fact: Fact }) {
   const [visible, setVisible] = useState(false);
 
@@ -35,26 +26,20 @@ export function FactCard({ fact }: { fact: Fact }) {
 export function FactLoader({ facts }: { facts: Fact[] }) {
   const [showAmount, setShowAmount] = useState(1);
 
-  const [shuffledFacts, setShuffledFacts] = useState<Fact[]>([]);
-
   useEffect(() => {
-    setShuffledFacts(shuffleFacts(facts));
-  }, [facts]);
-
-  useEffect(() => {
-    if (showAmount < shuffledFacts.length) {
+    if (showAmount < facts.length) {
       const timeout = setTimeout(
         () => {
           setShowAmount(c => c + 1);
         },
-        Math.max((100 * (shuffledFacts.length - showAmount)) / 100, 20)
+        Math.max((100 * (facts.length - showAmount)) / 100, 20)
       );
 
       return () => clearTimeout(timeout);
     }
-  }, [shuffledFacts.length, showAmount]);
+  }, [facts.length, showAmount]);
 
-  const visibleFacts = shuffledFacts.slice(0, showAmount);
+  const visibleFacts = facts.slice(0, showAmount);
 
   return (
     <>
--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,15 @@ export interface Fact {
   slug: string;
 }
 
+function shuffleFacts(facts: Fact[]) {
+  const newArrray = [...facts];
+  for (let i = newArrray.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [newArrray[i], newArrray[j]] = [newArrray[j], newArrray[i]];
+  }
+  return newArrray;
+}
+
 async function getFacts() {
   const csv = await readFile('./public/facts.csv', 'utf8');
 
@@ -15,14 +24,14 @@ async function getFacts() {
     skip_empty_lines: true
   }).toArray();
 
-  return facts;
+  return shuffleFacts(facts);
 }
 
 export default async function Home() {
   const facts = await getFacts();
 
   return (
-    <main className="grid grid-cols-1 sm:grid-cols-2 gap-x-24 gap-y-12 mx-auto my-10 w-3/4">
+    <main className="mx-auto my-10 w-3/4">
       <FactLoader facts={facts} />
     </main>
   );
