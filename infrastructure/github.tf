8012aff328f700ab04b20e51abb811f1e56c1130
Seamus Lowry <spl1owry3@gmail.com>
2024-12-24 10:46:04 +0000

    fix/dependabot secret (#63)
    * fix: dependabot secret
    
    * chore: newline at end of file

--- infrastructure/github.tf
+++ infrastructure/github.tf
@@ -3,3 +3,9 @@ resource "github_actions_secret" "webapp_api_token" {
   secret_name     = "AZURE_STATIC_WEB_APPS_API_TOKEN"
   plaintext_value = azurerm_static_web_app.realtruesciencefacts.api_key
 }
+
+resource "github_dependabot_secret" "webapp_api_token" {
+  repository      = "real-true-science-facts"
+  secret_name     = "AZURE_STATIC_WEB_APPS_API_TOKEN"
+  plaintext_value = azurerm_static_web_app.realtruesciencefacts.api_key
+}
